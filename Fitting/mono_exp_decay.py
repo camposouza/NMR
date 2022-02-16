29318e4fe6ed687ffc99fee14d820e20751c02dd
Guilherme de Souza Campos <2020021263>
2022-02-16 14:56:29 -0300

    Appending 'mono_decay_columns' into mono_exp_decay.py

--- Fitting/mono_exp_decay.py
+++ Fitting/mono_exp_decay.py
@@ -9,19 +9,28 @@ def main():
     # Reading results
     data = pd.read_table('data.txt', index_col=0, header=None)
     t = np.array(data.index)
-    y = np.array(data.iloc[:, 0])
+    for i in range(0, data.shape[1]):
+        y = np.array(data.iloc[:, 0])
 
-    fig = plt.figure()
-    ax1 = fig.add_subplot()
+        # Non-linear Fit
+        A, K, C = fit_exp_nonlinear(t, y)
+        fit_y = model_func(t, A, K, C)
 
-    # Non-linear Fit
-    A, K, C = fit_exp_nonlinear(t, y)
-    fit_y = model_func(t, A, K, C)
-    ax1.scatter(t, y, c='red', s=1,
-                label='Dados experimentais')
-    ax1.plot(t, fit_y, 'b-',
-             label='Curva encontrada:\n $y = %0.2f e^{%0.2f t} + %0.2f$' % (A, K, C))
-    ax1.legend(bbox_to_anchor=(1.05, 1.1), fancybox=True, shadow=True)
+        # Graphing Results
+        if i == 0:
+            fig = plt.figure()
+            ax1 = fig.add_subplot()
+
+            ax1.scatter(t, y, c='red', s=1,
+                        label='Dados experimentais')
+            ax1.plot(t, fit_y, 'b-',
+                     label='Curva encontrada:\n $y = %0.2f e^{%0.2f t} + %0.2f$' % (A, K, C))
+            ax1.legend(bbox_to_anchor=(1.05, 1.1), fancybox=True, shadow=True)
+
+        # Showing Results
+        print(f'Conjunto de dados {i + 1}:\n'
+              f'  A = {A:.2f}\n  K = {K:.2f}\n  C = {C:.2f}')
+
     plt.show()
 
 
