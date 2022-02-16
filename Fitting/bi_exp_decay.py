8390b436b671640afdaa245a7b74aff8594b0f6c
Guilherme de Souza Campos <2020021263>
2022-02-16 15:01:56 -0300

    Adding 'Showing Results'

--- Fitting/bi_exp_decay.py
+++ Fitting/bi_exp_decay.py
@@ -28,6 +28,11 @@ def main():
                            % (A1, K1, A2, K2, C))
             ax1.legend(bbox_to_anchor=(1.05, 1.1), fancybox=True, shadow=True)
 
+        # Showing Results
+        print(f'Conjunto de dados {i + 1}:\n'
+              f'  A1 = {A1:.2f}\n  K1 = {K1:.2f}\n'
+              f'  A2 = {A2:.2f}\n  K2 = {K2:.2f}\n  C = {C:.2f}\n')
+
     plt.show()
 
 
