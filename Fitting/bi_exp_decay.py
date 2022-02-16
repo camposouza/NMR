7bf23947f7b692447fdddd20e1d8ef25774a03f3
Guilherme de Souza Campos <2020021263>
2022-02-16 14:49:13 -0300

    Change on model function Introducing biexponential decay

--- Fitting/bi_exp_decay.py
+++ Fitting/bi_exp_decay.py
@@ -15,24 +15,27 @@ def main():
     ax1 = fig.add_subplot()
 
     # Non-linear Fit
-    A, K, C = fit_exp_nonlinear(t, y)
-    fit_y = model_func(t, A, K, C)
+    A1, K1, A2, K2, C = fit_exp_nonlinear(t, y)
+    fit_y = model_func(t, A1, K1, A2, K2, C)
+
+    # Showing Results
     ax1.scatter(t, y, c='red', s=1,
                 label='Dados experimentais')
     ax1.plot(t, fit_y, 'b-',
-             label='Curva encontrada:\n $y = %0.2f e^{%0.2f t} + %0.2f$' % (A, K, C))
+             label='Curva encontrada:\n $y = %0.2f e^{%0.2f t} + %0.2f e^{%0.2f t} + %0.2f$'
+                   % (A1, K1, A2, K2, C))
     ax1.legend(bbox_to_anchor=(1.05, 1.1), fancybox=True, shadow=True)
     plt.show()
 
 
-def model_func(t, A, K, C):
-    return A * np.exp((-1/K) * t) + C
+def model_func(t, A1, K1, A2, K2, C):
+    return A1 * np.exp((-1/K1) * t) + A2 * np.exp((-1/K2) * t) + C
 
 
 def fit_exp_nonlinear(t, y):
     opt_parms, parm_cov = sp.optimize.curve_fit(model_func, t, y, maxfev=1000)
-    A, K, C = opt_parms
-    return A, K, C
+    A1, K1, A2, K2, C = opt_parms
+    return A1, K1, A2, K2, C
 
 
 if __name__ == '__main__':
