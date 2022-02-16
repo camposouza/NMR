6fce7f78975cc34ada14762aa7eb3869db946295
Guilherme de Souza Campos <2020021263>
2022-02-16 15:44:01 -0300

    Adding to_csv() method

--- Fitting/bi_exp_decay.py
+++ Fitting/bi_exp_decay.py
@@ -9,6 +9,8 @@ def main():
     # Reading results
     data = pd.read_table('data.txt', index_col=0, header=None)
     t = np.array(data.index)
+
+    results = pd.DataFrame(index=['A1', 'K1', 'A2', 'K2', 'C'])
     for i in range(0, data.shape[1]):
         y = np.array(data.iloc[:, 0])
 
@@ -28,11 +30,10 @@ def main():
                            % (A1, K1, A2, K2, C))
             ax1.legend(bbox_to_anchor=(1.05, 1.1), fancybox=True, shadow=True)
 
-        # Showing Results
-        print(f'Conjunto de dados {i + 1}:\n'
-              f'  A1 = {A1:.2f}\n  K1 = {K1:.2f}\n'
-              f'  A2 = {A2:.2f}\n  K2 = {K2:.2f}\n  C = {C:.2f}\n')
+        # Filling the Sheet
+        results.insert(i, i, [A1, K1, A2, K2, C])
 
+    results.to_csv('FittingResults', sep='\t')
     plt.show()
 
 
